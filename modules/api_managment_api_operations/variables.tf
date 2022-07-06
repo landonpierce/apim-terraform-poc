13911d73fae15272ba82c957176f96ef1b3c82a6
Landon Pierce <landonpierce@microsoft.com>
2022-07-06 16:50:51 -0400

    more work

---
+++ modules/api_managment_api_operations/variables.tf
@@ -0,0 +1,19 @@
+variable "resource_group_name" {
+    type = string
+}
+
+variable "api_management_name" {
+    description = "The name of the API Management service to create the api under."
+    type = string
+}
+
+
+variable "api_name" {
+    description = "The name of the API to create."
+    type = string
+}
+
+variable "resource_name" {
+    description = "The path of the API Resource. Ex: /orders, /customers/orders, /products, etc."
+    type = string
+}
